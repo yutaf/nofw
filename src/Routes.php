f488f844764265703d0a1c6906e92916d5a907bb
yutaf <yutafuji2008@gmail.com>
2016-05-22 14:00:21 +0900

    Create src/Routes.php

---
+++ src/Routes.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    ['GET', '/hello-world', function () {
+        echo 'Hello World';
+    }],
+    ['GET', '/another-route', function () {
+        echo 'This works too';
+    }],
+];
