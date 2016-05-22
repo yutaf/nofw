d1f6e73321aabe2aeb2e8326bcf0ed7650c38962
yutaf <yutafuji2008@gmail.com>
2016-05-22 11:27:45 +0900

    Define $environment from environment value

--- src/bootstrap.php
+++ src/bootstrap.php
@@ -6,19 +6,19 @@ require __DIR__ . '/../vendor/autoload.php';
 
 error_reporting(E_ALL);
 
-$environment = 'development';
+//$environment = 'development';
 //$environment = 'production';
 
 /**
  * Register the error handler
  */
 $whoops = new \Whoops\Run;
-if ($environment !== 'production') {
-    $whoops->pushHandler(new \Whoops\Handler\PrettyPageHandler);
-} else {
+if (getenv('ENVIRONMENT') === 'production') {
     $whoops->pushHandler(function($e){
         echo 'Friendly error page and send an email to the developer';
     });
+} else {
+    $whoops->pushHandler(new \Whoops\Handler\PrettyPageHandler);
 }
 $whoops->register();
 
